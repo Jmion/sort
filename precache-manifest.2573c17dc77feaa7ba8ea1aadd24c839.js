b9a6eb24c492234ac5111c5e186995bfd859e18f
Jeremy Mion <jeremy.mion@yahoo.ca>
2019-10-07 00:46:00 +0200

    Updates

---
+++ precache-manifest.2573c17dc77feaa7ba8ea1aadd24c839.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a58323caa460dcb6455c1f2ac6eec48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c47640ca49707b3f9807",
+    "url": "/static/css/2.d7a94725.chunk.css"
+  },
+  {
+    "revision": "50573626bdf6f8b71a6a",
+    "url": "/static/css/main.6823ddd8.chunk.css"
+  },
+  {
+    "revision": "c47640ca49707b3f9807",
+    "url": "/static/js/2.1c2c3fc0.chunk.js"
+  },
+  {
+    "revision": "50573626bdf6f8b71a6a",
+    "url": "/static/js/main.a0e380a5.chunk.js"
+  },
+  {
+    "revision": "5ab0924ce41da84de2d3",
+    "url": "/static/js/runtime-main.13e7593c.js"
+  },
+  {
+    "revision": "e575707bdab1e2ba3174205a8ee422fa",
+    "url": "/static/media/color_background.e575707b.jpg"
+  },
+  {
+    "revision": "3783d046f17cc631fe29309decb4c427",
+    "url": "/static/media/corrosion.3783d046.svg"
+  },
+  {
+    "revision": "9b1dea1d474fd6fe7979ff9fe438e89a",
+    "url": "/static/media/environment.9b1dea1d.svg"
+  },
+  {
+    "revision": "11dddf4fcc30c18533744f3e3ecb1388",
+    "url": "/static/media/exclamation_mark.11dddf4f.svg"
+  },
+  {
+    "revision": "fad7e4b03c58ca6a39a7d8d4ed91e98a",
+    "url": "/static/media/exploding_bomb.fad7e4b0.svg"
+  },
+  {
+    "revision": "07f6ed651b237f9f57bcd037de1db050",
+    "url": "/static/media/flamable.07f6ed65.svg"
+  },
+  {
+    "revision": "9c48680cf666f9a05b4192e792a03f6a",
+    "url": "/static/media/gas_cylinder.9c48680c.svg"
+  },
+  {
+    "revision": "7c8930e6b84afba4946c47e6f515c3c5",
+    "url": "/static/media/health_hazard.7c8930e6.svg"
+  },
+  {
+    "revision": "61dcc44a3420fbd29251d17c45d17c63",
+    "url": "/static/media/logo.61dcc44a.png"
+  },
+  {
+    "revision": "ad5855b0fda4a5040c1584c79d6e9171",
+    "url": "/static/media/oxidizer.ad5855b0.svg"
+  },
+  {
+    "revision": "d5734eb60287e81f174a549e128878fa",
+    "url": "/static/media/skull.d5734eb6.svg"
+  }
+]);
